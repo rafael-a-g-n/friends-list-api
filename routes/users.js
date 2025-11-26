332f73df5581dff66ea7441605c76a35f89be55b
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-26 16:00:33 +0000

    Implemented user deletion and standardize GET response

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ let users = [
 // GET request: Retrieve all users
 router.get("/", (req, res) => {
   // Copy the code here
-  res.send(users);
+  res.send(JSON.stringify({ users }, null, 4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
@@ -83,8 +83,11 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  const email = req.params.email;
+
+  users = users.filter((user) => user.email !== email);
+
+  res.send(`User with the email ${email} has been deleted`);
 });
 
 module.exports = router;
